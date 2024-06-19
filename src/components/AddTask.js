7bda92e10983be1b0452b1a7d16c59740c511d91
sriniharika <niharika@gmail.com>
2024-06-19 12:06:47 +0530

    13th commit

--- src/components/AddTask.js
+++ src/components/AddTask.js
@@ -3,8 +3,11 @@ import "./addTask.css";
 const AddTask = () => {
   return (
     <section className="addTask">
+        <form>
         <label htmlFor="task">Task Name:</label>
         <input type="text" id="task" name="task"></input>
+        <button type="submit">ADD TASK</button>
+        </form>
     </section>
   )
 }
