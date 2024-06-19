5c258271f8cd3adcab3fee586080bfd0ca31fa98
sriniharika <niharika@gmail.com>
2024-06-19 12:21:00 +0530

    14th commit

--- src/components/AddTask.js
+++ src/components/AddTask.js
@@ -5,7 +5,7 @@ const AddTask = () => {
     <section className="addTask">
         <form>
         <label htmlFor="task">Task Name:</label>
-        <input type="text" id="task" name="task"></input>
+        <input type="text" id="task" name="task" placeholder="Enter Task name" autoComplete="off"></input>
         <button type="submit">ADD TASK</button>
         </form>
     </section>
