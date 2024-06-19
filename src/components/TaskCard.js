aa0e3a644550c4357a5db4b4f1f61769e7796eb6
sriniharika <niharika@gmail.com>
2024-06-19 11:35:57 +0530

    11th commit

--- src/components/TaskCard.js
+++ src/components/TaskCard.js
@@ -1,10 +1,11 @@
 import "./taskCard.css";
+import styles from "./TaskCard.module.css";
 
 const TaskCard = ({ task, handleDel }) => {
   return (
     <div>
       <li className={task.completed ? "completed" : "incomplete"}>
-        <span>
+        <span className={styles.title}>
           {task.id} - {task.Name}
         </span>
         <button onClick={() => handleDel(task.id)}>Delete</button>
